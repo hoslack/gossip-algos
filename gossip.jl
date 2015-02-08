88aa06274c0812af420fc23c071a192e6bdfd155
Howon Lee <howon@howonlee.com>
2015-02-08 01:07:33 -0800

    outline

---
+++ gossip.jl
@@ -0,0 +1,22 @@
+using Graphs
+
+function gossip_minimum(graph)
+  #each node has a value and its own guess for minimum
+  #at each point, the node takes a random neighbor and
+  #compare the guess it has and its neighbor's guess and take min
+end
+
+function gossip_minimum_test(graph)
+end
+
+function gossip_sum(graph)
+  #each node has a value and its own guess for minimum
+  #at each point, the node takes a random neighbor and
+  #compare the guess it has and its neighbor's guess and take min
+  #then generate some random numbers according to exponential distribution
+  #then actually say that the mean is the estimator,
+  #due to properties of the exponential distribution. hey! you have mean, so you have sum!
+end
+
+function gossip_sum_test(graph)
+end
